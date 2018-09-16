9106a8503556eee4fa6959ad29de447a9db613d2
Jaehyun Ahn <jaehyunahn@sogang.ac.kr>
2018-09-16 23:38:35 +0900

    ready for movie container

--- js/header.js
+++ js/header.js
@@ -1,8 +1,19 @@
-/*global $, window */
+/*global $, window, console, document */
 
+/* when hamburger opened */
 $(window).on("load", function () {
     "use strict";
     $('#hamburger').click(function () {
         $('#hamburger').toggleClass('open');
+    });
+
+    /* scroll 시 TOP 애니메이션 설정 */
+    $(window).scroll(function () {
+        var height = $(document).scrollTop();
+        if (height <= 100) {
+            $('#header-container').removeClass('active');
+        } else {
+            $('#header-container').addClass('active');
+        }
     });
 });
