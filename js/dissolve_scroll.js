329850ab876699541b4c2c01ab1184ade635715a
Jaehyun Ahn <jaehyunahn@sogang.ac.kr>
2018-09-30 23:11:46 +0900

    video js for play in chrome

--- js/dissolve_scroll.js
+++ js/dissolve_scroll.js
@@ -1,4 +1,4 @@
-/*global $, window, document, console */
+/*global $, window, document */
 
 /* when hamburger opened */
 $(window).on("load", function () {
@@ -6,25 +6,32 @@ $(window).on("load", function () {
     $('#hamburger').click(function () {
         $('#hamburger').toggleClass('open');
     });
+    
+    var half12_flag = true;
 
     /* scroll 시 TOP 애니메이션 설정 */
     $(window).scroll(function () {
-        var height = $(document).scrollTop();
-        var first_container = $('#first-container').position().top;
-        var second_minerva = $('#second_minerva').position().top;
-        var half12 = (second_minerva - first_container) / 3;
+        var height = $(document).scrollTop(),
+            first_container = $('#first-container').position().top,
+            second_minerva = $('#second_minerva').position().top,
+            half12 = (second_minerva - first_container) / 3;
         
         if (height >= half12) {
             // desolve 2 items
             // left-container, minerva_logo, 2_title, 2_text
+            if (half12_flag) {
+                $('.left-container').children('img').animate({left: "+=100"}, 1000);
+                half12_flag = false;
+            }
+            
             $('.left-container').fadeIn(1000);
-            $('#minerva_logo').fadeIn(1000, function() {
-               $('#title_2').fadeIn(1000, function() {
-                   $('#text_2').fadeIn(1000, function() {
-                      $('#button_2').fadeIn(1500); 
-                   });
-                }); 
-            });        
+            $('#minerva_logo').fadeIn(1000, function () {
+                $('#title_2').fadeIn(1000, function () {
+                    $('#text_2').fadeIn(1000, function () {
+                        $('#button_2').fadeIn(1500);
+                    });
+                });
+            });
         }
     });
 });
