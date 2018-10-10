81b1b142464935ddb6ea7f2f445479b086e453b7
Jaehyun Ahn <jaehyunahn@sogang.ac.kr>
2018-10-10 15:19:29 +0900

    galleries settings

---
+++ js/swiper.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var swiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  });
