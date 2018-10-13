9b1d9d97922bb4bce8a2b7a7b633fb0b0b109da2
Jaehyun Ahn <sogosonnet@gmail.com>
2018-10-13 18:47:36 +0900

    delete bootstrap

--- js/swiper.js
+++ js/swiper.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
     //initialize swiper when document ready
     var swiper = new Swiper('.swiper-container', {
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        speed: 300,
     });
   });
